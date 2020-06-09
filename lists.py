82c793394d0750f3737bd6ab808857ccdc3ee28d
Paul Fann <paul.fann74@gmail.com>
2020-06-09 14:14:21 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,8 @@
+# friends1 = ["Jim", 2, false]
+
+friends = ["Jim", "Bob", "Fred", "Mary", "John", "Shaun"]
+friends[1] = "changed from Bob"
+print(friends[-1])
+print(friends[1:])
+print(friends[2:4])
+print(friends[1])
